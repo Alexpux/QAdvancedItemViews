195bd129080e8f5b7ad8c6b376df86c4d2e7b0a9
Thomas Petermann <tpetermann68@googlemail.com>
2012-06-11 10:00:02 +0200

    Added setItemDelegateForColumn, setItemDelegateForRow methods to class QAdvancedTableView.

--- qaivlib/qadvancedtableview.cpp
+++ qaivlib/qadvancedtableview.cpp
@@ -553,6 +553,16 @@ void QAdvancedTableView::setItemDelegate(QAbstractItemDelegate* delegate)
 	ui->dataTableView->setItemDelegate(delegate);
 }
 
+void QAdvancedTableView::setItemDelegateForColumn(int column, QAbstractItemDelegate * delegate)
+{
+	ui->dataTableView->setItemDelegateForColumn(column, delegate);
+}
+
+void QAdvancedTableView::setItemDelegateForRow(int row, QAbstractItemDelegate * delegate)
+{
+	ui->dataTableView->setItemDelegateForRow(row, delegate);
+}
+
 void QAdvancedTableView::setGridStyle(Qt::PenStyle style)
 {
     ui->headerTableView->setGridStyle(style);
