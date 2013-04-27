8dd5d4b403aadf4ef3f421f1b2855fbc8e58a898
MacHope <MacHope@localhost>
2013-04-27 08:12:46 +0200

    copyright notice added

--- qaivdesigner/qaivdesignercollection.cpp
+++ qaivdesigner/qaivdesignercollection.cpp
@@ -1,10 +1,33 @@
+/******************************************************************************
+** This file is part of QAIV (QAdvanced Item Views).
+**
+** Copyright (c) 2011-2013 Martin Hoppe martin@2x2hoppe.de
+**
+** qadvanceditemviews is free software: you can redistribute it
+** and/or modify it under the terms of the GNU Lesser General
+** Public License as published by the Free Software Foundation,
+** either version 3 of the License, or (at your option) any
+** later version.
+**
+** qadvanceditemviews is distributed in the hope that it will be
+** useful, but WITHOUT ANY WARRANTY; without even the implied
+** warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with qadvanceditemviews.
+** If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 #include "qaivdesignercollection.h"
+
 #include "qadvancedtableviewplugin.h"
+#include "qfixedrowstableviewplugin.h"
 
 QAIVDesignerCollection::QAIVDesignerCollection(QObject *parent) :
     QObject(parent)
 {
     m_widgets.append(new QAdvancedTableViewPlugin());
+	m_widgets.append(new QFixedRowsTableViewPlugin());
 }
 
 QList<QDesignerCustomWidgetInterface *> QAIVDesignerCollection::customWidgets() const
--- qaivdesigner/qaivdesignercollection.h
+++ qaivdesigner/qaivdesignercollection.h
@@ -1,8 +1,28 @@
+/******************************************************************************
+** This file is part of QAIV (QAdvanced Item Views).
+**
+** Copyright (c) 2011-2013 Martin Hoppe martin@2x2hoppe.de
+**
+** qadvanceditemviews is free software: you can redistribute it
+** and/or modify it under the terms of the GNU Lesser General
+** Public License as published by the Free Software Foundation,
+** either version 3 of the License, or (at your option) any
+** later version.
+**
+** qadvanceditemviews is distributed in the hope that it will be
+** useful, but WITHOUT ANY WARRANTY; without even the implied
+** warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with qadvanceditemviews.
+** If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 #ifndef QAIVDESIGNERCOLLECTION_H
 #define QAIVDESIGNERCOLLECTION_H
 
 #include <QtDesigner/QtDesigner>
- #include <QtCore/qplugin.h>
+#include <QtCore/qplugin.h>
 
 class QAIVDesignerCollection : public QObject, public QDesignerCustomWidgetCollectionInterface
 {
